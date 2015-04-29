72be6b3db71377d783ab21e6bfe1d5de4fb65866
michalmasir <michal.masir@gmail.com>
2015-04-30 01:12:47 +0200

    Upraveny bootsrap vzhlad + pridana datova tabulka, nieje treba sortovat data...

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,21 +5,25 @@ namespace App\Presenters;
 use Nette,
     App\Model;
 
-
 /**
  * Homepage presenter.
  */
-class HomepagePresenter extends BasePresenter
-{
-        public function __construct(Nette\Database\Context $database)
-        {
-            parent::__construct($database);
-        }
+class HomepagePresenter extends BasePresenter {
+
+    public function __construct(Nette\Database\Context $database) {
+        parent::__construct($database);
         
-	public function renderDefault()
-	{
-            $this->template->tovary = $this->database->table('tovar')
-                ->limit('10');
-	}
+    }
+
+    public function renderDefault() {
+        if (!$this->getUser()->isLoggedIn()) {
+            //$this->error('Pro vytvoření, nebo editování příspěvku se musíte přihlásit.');
+            $this->flashMessage('Musíte sa prihlásiť !');
+            //$this->redirect('Homepage:');
+        } else {
+            $this->template->tovary = $this->database->table('tovar');
+            // $this->database->
+        }
+    }
 
 }
--- app/presenters/SignPresenter.php
+++ app/presenters/SignPresenter.php
@@ -3,62 +3,59 @@
 namespace App\Presenters;
 
 use Nette,
-    App\Forms\SignFormFactory,  Nette\Application\UI\Form;
-
+    App\Forms\SignFormFactory,
+    Nette\Application\UI\Form;
 
 /**
  * Sign in/out presenters.
  */
-class SignPresenter extends BasePresenter
-{
-	/** @var SignFormFactory @inject */
-	public $factory;
+class SignPresenter extends BasePresenter {
+
+    /** @var SignFormFactory @inject */
+    public $factory;
+    public $form;
+    
+    public function __construct(Nette\Database\Context $database) {
+        parent::__construct($database);
         
-        
-        public function __construct(Nette\Database\Context $database)
-        {
-            parent::__construct($database);
-        }
-        
-	/**
-	 * Sign-in form factory.
-	 * @return Nette\Application\UI\Form
-	 */
+    }
+
+    /**
+     * Sign-in form factory.
+     * @return Nette\Application\UI\Form
+     */
     protected function createComponentSignInForm() {
         $form = new Form();
         $form->addText('username', 'Uživatelské jméno:')
-                ->setRequired('Prosím vyplňte své uživatelské jméno.');
+                ->setRequired('Prosím vyplňte své uživatelské jméno.')->getControlPrototype()->setClass('form-control');
 
         $form->addPassword('password', 'Heslo:')
-                ->setRequired('Prosím vyplňte své heslo.');
+                ->setRequired('Prosím vyplňte své heslo.')->getControlPrototype()->setClass('form-control');
 
-        $form->addCheckbox('remember', 'Zůstat přihlášen');
+        $form->addCheckbox('remember', 'Zůstat přihlášen')->getControlPrototype()->setClass('form-control');
 
-        $form->addSubmit('send', 'Přihlásit');
-
-        $form->onSuccess[] = array($this, 'signInFormSucceeded');
-        return $form;
+        $form->addSubmit('send', 'Přihlásit')->getControlPrototype()->setClass('btn-primary');
+  
+        $this->form = $form;
+        $this->form->onSuccess[] = array($this, 'signInFormSucceeded');
+        return $this->form;
     }
 
-        public function signInFormSucceeded($form)
-        {
-            $values = $form->values;
+    public function signInFormSucceeded($form) {
+        $values = $form->values;
 
-            try {
-                $this->getUser()->login($values->username, $values->password);
-                $this->redirect('Homepage:');
-
-            } catch (Nette\Security\AuthenticationException $e) {
-                $form->addError('Nesprávné přihlašovací jméno nebo heslo.');
-            }
-        }
-        
-
-	public function actionOut()
-        {
-            $this->getUser()->logout();
-            $this->flashMessage('Odhlášení bylo úspěšné.');
+        try {
+            $this->getUser()->login($values->username, $values->password);
             $this->redirect('Homepage:');
+        } catch (Nette\Security\AuthenticationException $e) {
+            $form->addError('Nesprávné přihlašovací jméno nebo heslo.');
         }
+    }
+
+    public function actionOut() {
+        $this->getUser()->logout();
+        $this->flashMessage('Odhlášení bylo úspěšné.');
+        $this->redirect('Homepage:');
+    }
 
 }
