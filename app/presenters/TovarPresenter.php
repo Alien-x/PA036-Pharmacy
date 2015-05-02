6f2a3ce215f6553e6c7812932b50fe1022ac76e1
Misko <Misko@Michal-pc>
2015-05-02 15:22:40 +0200

    selekt box hotovo

--- app/presenters/TovarPresenter.php
+++ app/presenters/TovarPresenter.php
@@ -11,11 +11,9 @@ class TovarPresenter extends BasePresenter {
     private $tovar;
     private $db;
 
-
+    
     public function __construct(Nette\Database\Context $database) {
         $this->db = $database;
-
-
     }
 
     /** startup */
@@ -25,7 +23,26 @@ class TovarPresenter extends BasePresenter {
     }
 
     public function renderDefault() {
-        $this->template->tovary = $this->tovar->printAll();
+        $post = $this->getHttpRequest()->getPost();
+
+        $this->template->post = $post;
+        if (isset($post['indikacna_skupina']) && $post['indikacna_skupina']!=null && $post['indikacna_skupina']!=0)
+            $this->template->tovary = $this->db->query('select t.id_tovar,t.nazov, t.cena, t.na_predpis, t.doplatok, t.popis, t.doplnkovy_tovar, t.aktivny, t.pocet, t.drzitel, id_skupina , ol.id_ucinna 
+                from tovar t 
+                LEFT OUTER JOIN liek l ON(l.id_tovar = t.id_tovar) 
+                JOIN obsah_latok ol ON(ol.id_liek = l.id_liek)
+                where id_skupina = ?', $post['indikacna_skupina']);
+        else if(isset($post['indikacna_skupina']) && $post['indikacna_skupina']==0)
+            $this->template->tovary = $this->db->query("select t.id_tovar,t.nazov, t.cena, t.na_predpis, t.doplatok, t.popis, t.doplnkovy_tovar, t.aktivny, t.pocet, t.drzitel, id_skupina , ol.id_ucinna 
+                from tovar t 
+                LEFT OUTER JOIN liek l ON(l.id_tovar = t.id_tovar) 
+                JOIN obsah_latok ol ON(ol.id_liek = l.id_liek)
+                and t.doplnkovy_tovar = true" );
+        else
+            $this->template->tovary = $this->db->query('select t.id_tovar,t.nazov, t.cena, t.na_predpis, t.doplatok, t.popis, t.doplnkovy_tovar, t.aktivny, t.pocet, t.drzitel, id_skupina , ol.id_ucinna 
+                from tovar t 
+                LEFT OUTER JOIN liek l ON(l.id_tovar = t.id_tovar) 
+                JOIN obsah_latok ol ON(ol.id_liek = l.id_liek)');
     }
 
     public function renderShow($id_tovar) {
@@ -33,20 +50,18 @@ class TovarPresenter extends BasePresenter {
     }
 
     protected function createComponentSkupinaForm() {
+        
         $skupina = $this->db->table('indikacna_skupina')->fetchPairs('id_skupina', 'nazov');
-        $skupina[] = ('Doplnky');
+        array_unshift($skupina, 'Doplnky');
+        
         $form = new Form;
-        $form->addSelect('indikacna_skupina', 'Skupina tovaru:')->setItems($skupina)->setPrompt('Vsetko');
+        $form->addSelect('indikacna_skupina', 'Skupina tovaru:')->setItems($skupina)->setPrompt('Vsetko')->getControlPrototype()->setClass('form-control');
         $form->addSubmit('choose', 'Ok')->getControlPrototype()->setClass('btn btn-primary');
         $form->onSuccess[] = array($this, 'skupinaFormSucceeded');
         return $form;
     }
 
     public function skupinaFormSucceeded($form, $values) {
-       $post = $this->getHttpRequest()->getPost();
-       $this->template->post = $post;
-       //if(isset($post['indikacna_skupina']))
-          // $this->template->tovary = $this->tovar->
     }
 
 }
