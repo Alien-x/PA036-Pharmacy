f5afa8ef7832b38bf8e8d0b2b8e86bc9ae16ffc9
michalmasir <michal.masir@gmail.com>
2015-05-05 10:24:21 +0200

    validacia insert formu

--- app/presenters/SkladPresenter.php
+++ app/presenters/SkladPresenter.php
@@ -3,7 +3,7 @@
 namespace App\Presenters;
 
 use Nette,
-    Nette\Application\UI\Form;
+    Nette\Application\UI\Form, Nette\Forms\Validator;
 
 class SkladPresenter extends BasePresenter {
 
@@ -31,13 +31,13 @@ class SkladPresenter extends BasePresenter {
         //obecny formular
         //
         $form->addText('nazov', 'Názov: ')->setRequired();
-        $form->addText('cena', 'Cena: ')->setRequired();
+        $form->addText('cena', 'Cena: ')->addRule(Form::FLOAT, "§§§§");
         $form->addCheckbox('na_predpis', 'Na predpis: ');
-        $form->addText('doplatok', 'Doplatok: ');
+        $form->addText('doplatok', 'Doplatok: ')->addConditionOn($form['na_predpis'], Form::EQUAL, TRUE)->setRequired('Vyberte jednu zo skupín !');
         $form->addText('popis', 'Popis: ')->setRequired();
         $form->addText('min_pocet', 'Minimálny počet: ')->setRequired();
-        $form->addText('doplnkovy_tovar', 'Doplnkový tovar: ')->setRequired();
-        $form->addCheckbox('aktivny', 'Aktívny: ')->setRequired();
+        $form->addCheckbox('doplnkovy_tovar', 'Doplnkový tovar: ');
+        $form->addCheckbox('aktivny', 'Aktívny: ');
         $form->addSelect('id_forma', 'Forma tovaru: ')->setItems($mnozstvo_forma)->setPrompt('Vyberte jednu')->setRequired();
         $form->addText('mnozstvo', 'Množstvo: ')->setRequired();
         $form->addText('drzitel', 'Držitel: ')->setRequired();
@@ -45,8 +45,10 @@ class SkladPresenter extends BasePresenter {
         $form->addText('pocet', 'Počet: ')->setRequired();
         //formular pre liek
         $form->addCheckbox('je_liek', 'Je liek: ');
-        $form->addSelect('id_skupina', 'Skupina tovaru:')->setItems($skupina)->setPrompt('Vyberte jednu');
-        $form->addSelect('id_ucinna', 'Ucinna latka lieku: ')->setItems($ucinne_latky)->setPrompt('Vyberte jednu');
+        $form->addSelect('id_skupina', 'Skupina tovaru:')->setItems($skupina)->setPrompt('Vyberte jednu')
+                ->addConditionOn($form['je_liek'], Form::EQUAL, TRUE)->setRequired('Vyberte jednu zo skupín !');
+        $form->addSelect('id_ucinna', 'Ucinna latka lieku: ')->setItems($ucinne_latky)->setPrompt('Vyberte jednu')
+                ->addConditionOn($form['je_liek'], Form::EQUAL, TRUE)->setRequired('Vyberte jednu zo skupín !');
 
 
         $form->addSubmit('insert', 'Pridaj')->getControlPrototype()->setClass('form-control btn btn-primary');
@@ -55,6 +57,7 @@ class SkladPresenter extends BasePresenter {
     }
 
     public function insertFormSucceeded($form, $values) {
+        $this->template->values = $form->values;
         
     }
 
